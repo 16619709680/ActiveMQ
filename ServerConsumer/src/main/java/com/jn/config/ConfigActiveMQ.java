05e3ca7fe45f868f0fb4afa540cea79539450912
16619709680 <66500755+16619709680@users.noreply.github.com>
2020-07-30 22:49:16 +0800

    AMQ 分布式事务处理案例Demo

---
+++ ServerConsumer/src/main/java/com/jn/config/ConfigActiveMQ.java
@@ -0,0 +1,11 @@
+package com.jn.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ConfigActiveMQ {
+
+
+
+}
